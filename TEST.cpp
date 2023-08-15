4c29dad167407c29f231f01249323b80db8e5f84
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-08-15 20:05:35 +0200

    Add files via upload

---
+++ TEST.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    while (n != 42)
+          if (n == 42) {
+                cout << endl;
+          }elif (n == "No number") {
+          	    cout << "No number";
+          }else{
+                cout << n;
+    cin >> n;      
+}
+return 0;
+}
