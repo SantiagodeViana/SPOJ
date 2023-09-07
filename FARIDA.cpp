33278cf37a039dd1e51af83c15bf20bcf91748bc
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-09-08 01:18:09 +0200

    Add files via upload

---
+++ FARIDA.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int max_caramelo (int indice, bool puedo){
+    if (indice == n) return 0;
+    if (!puedo) return max_caramelo (indice +1, true);
+    return dp [indice][puedo] (max_caramelo (indice +1, true));
+           max_caramelo (indice +1, false) + caramelo [indice]);
+int main (){
+
+    return 0;
+}
