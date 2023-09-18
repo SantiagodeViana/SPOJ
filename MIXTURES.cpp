31ee060822a1f2cc858a585bd45377fa24cdbc6c
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-09-18 13:52:57 +0200

    Add files via upload

---
+++ MIXTURES.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int mejor (int desde, int hasta){
+    if (desde == hasta) return 0;
+    for (int corte = desde + 1){
+        corte < hasta;
+        corte++
+    }
+    int hiq = mejorr (desde, corte)
+    int hder = mejor(corte, hasta)
+    int hnuevo = (hiza + hder + (pocion[desde][corte] + pocion[corte][desde]);
+    if (hnuevo < mejor_humo){
+       mejor_humo = hnuevo;
+       pocion(desde)(hasta) = (pocion(desde)(corte) + pocion(corte)(hasta) % 100)
+    }
+}
+return mejor_humo;
+return 0;
+}
