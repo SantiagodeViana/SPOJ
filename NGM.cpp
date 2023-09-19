3d66443c84f02da58baa15f583ee6edd918d80ec
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-09-19 08:47:07 +0200

    Add files via upload

---
+++ NGM.cpp
@@ -0,0 +1,12 @@
+#include <iostream>;
+using namespace std;
+
+int main (){
+    int n;
+    cin >> n;
+    int moves;
+    #(n / (10^veces)) %10
+    #if 0: cout << 2
+    #else: cout << 1
+    return 0;
+}
