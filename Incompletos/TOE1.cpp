dabd7d28ef9c14c1dd7aad005f49c82cfd8a5ae7
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-09-24 11:54:15 +0200

    Add files via upload

---
+++ Incompletos/TOE1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>;
+using namaspace std;
+
+bool winner (x){
+     #si gana x, +1
+     #Si es igual, = ó +1
+     #Si gana 0, =
+}
+
+int main (){
+    int n;
+    cin >> n
+    for (int i = 0; i < n; i ++){
+        char game [2][2];
+        cin >> game [0][0] >> game [0][1] >> game [0][2];
+        cin >> game [1][0] >> game [1][1] >> game [1][2];
+        cin >> game [2][0] >> game [2][1] >> game [2][2];              
+        if game [i][0] >> game [i][1] >> game [i][2] || game [0][i] >> game [1][i] >> game [2][i] || game [0][0] >> game [1][1] >> game [2][2] || game [0][2] >> game [1][1] >> game [2][0];
+        
+        if winner != loser + 1{
+           cout << "no";
+        }else{
+           cout << "yes";   
+    }return 0;
+}
