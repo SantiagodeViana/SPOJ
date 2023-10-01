75304b5bdcecb2f8216b3561c9ba3d0082858ee1
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-10-02 00:07:05 +0200

    Add files via upload

---
+++ Incompletos/TRT.cpp
@@ -0,0 +1,6 @@
+int mentos (int inicio, int final, ind dia)
+    if (dia == n) return 0;
+    else{
+         return (m[inicio*dia + mentos (inicial +1, final, dia+1);
+                m[inicio*dia + mentos (inicial +1, final, dia+1);
+                
